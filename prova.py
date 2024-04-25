d002b1ea2162b52e5b021c0b8a582a01405149b6
Bianca <bianca@morgari.it>
2024-04-25 12:11:39 +0200

    miao3

--- prova.py
+++ prova.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import math
+#import math
 from gurobipy import Model, GRB, quicksum, Env
 
 # %%
@@ -15,13 +15,22 @@ W=pd.read_excel('data.xlsx',sheet_name='Wind')
 
 # prepare data into clean single days...
 # potentially start at midday?
-el = np.matrix(EL.loc[EL["Quarter"]=="Q1"].groupby("Instance")["Load"].sum().to_list())
-gl = np.matrix(GL.loc[GL["Quarter"]=="Q1"]["Load"].tolist())
-s = np.matrix(S.loc[S["Quarter"]=="Q1"]["Generation"].tolist())
-w = np.matrix(W.loc[W["Quarter"]=="Q1"]["Generation"].tolist())
+el=[]
+gl=[]
+s=[]
+w=[]
+for i in range(4):
+    el = el + [EL.loc[EL["Quarter"]=="Q{}".format(i+1)].groupby("Instance")["Load"].sum().to_list()]
+    gl = gl + [GL.loc[GL["Quarter"]=="Q{}".format(i+1)]["Load"].tolist()]
+    s = s + [S.loc[S["Quarter"]=="Q{}".format(i+1)]["Generation"].tolist()]
+    w = w + [W.loc[W["Quarter"]=="Q{}".format(i+1)]["Generation"].tolist()]
+el = np.matrix(el)
+gl = np.matrix(gl)
+s = np.matrix(s)
+w = np.matrix(w)
 
 # %%
-def OPT(ES,EW,EL,HL,cs=10000,cw=1000000,ch=10,chte=0,fhte=1,mhte=10,ceth=0,feth=1,meth=10):
+def OPT(ES,EW,EL,HL,cs=10000,cw=1000000,ch=10000,chte=0,fhte=1,mhte=10,ceth=0,feth=1,meth=10):
     #cost of solar panels
     #cost of wind turbines
     #cost of hydrogen storage???
@@ -36,22 +45,19 @@ def OPT(ES,EW,EL,HL,cs=10000,cw=1000000,ch=10,chte=0,fhte=1,mhte=10,ceth=0,feth=
     #electricity load - dx96 array
     #hydrogen load - dx96 array
     
-    d=len(ES)
+    d,inst=np.shape(ES)
     env = Env(params={'OutputFlag': 0})
     model = Model(env=env)
     
     ns = model.addVar(vtype=GRB.INTEGER, obj=cs,lb=0)
     nw = model.addVar(vtype=GRB.INTEGER, obj=cw,lb=0)    
     nh = model.addVar(vtype=GRB.CONTINUOUS, obj=ch,lb=0) #integer?    
-    HtE = model.addMVar((d,96),vtype=GRB.CONTINUOUS, obj=chte,lb=0) # expressed in kg      
-    EtH = model.addMVar((d,96),vtype=GRB.CONTINUOUS, obj=ceth, lb=0) # expressed in power
-    H = model.addMVar((d,96),vtype=GRB.CONTINUOUS,lb=0)
+    HtE = model.addMVar((d,inst),vtype=GRB.CONTINUOUS, obj=chte,lb=0) # expressed in kg      
+    EtH = model.addMVar((d,inst),vtype=GRB.CONTINUOUS, obj=ceth, lb=0) # expressed in power
+    H = model.addMVar((d,inst),vtype=GRB.CONTINUOUS,lb=0)
 
     for j in range(d):
-        for i in range(96):
-        #model.addConstr(np.eye(d),EL[:,i] + EtH[:,i], < , fhte*HtE[:,i] + ns*ES[:,i] + nw*EW[:,i] )
-        #model.addMConstr( H[:,i] = H[:,i-1] + feth*EtH[:,i] - HL[:,i] - HtE[:,i] )
-        #model.addConstr(np.eye(d), H[:,i],<,nh*np.ones((d,1)) )
+        for i in range(inst):
             model.addConstr( EL[j,i] + EtH[j,i] <= fhte*HtE[j,i] + ns*ES[j,i] + nw*EW[j,i] )
             model.addConstr( H[j,i] == H[j,i-1] + feth*EtH[j,i] - HL[j,i] - HtE[j,i] )
             model.addConstr( H[j,i] <= nh )
@@ -62,12 +68,14 @@ def OPT(ES,EW,EL,HL,cs=10000,cw=1000000,ch=10,chte=0,fhte=1,mhte=10,ceth=0,feth=
     return print(string)
 
 # %%
-el = EL.loc[EL["Quarter"]=="Q1"].groupby("Instance")["Load"].sum().to_list()
-gl = GL.loc[GL["Quarter"]=="Q1"]["Load"].tolist()
-s = S.loc[S["Quarter"]=="Q1"]["Generation"].tolist()
-w = W.loc[W["Quarter"]=="Q1"]["Generation"].tolist()
+el1 = np.matrix(EL.loc[(EL["Quarter"]=="Q2") & (EL["Instance"] >= 48)].groupby("Instance")["Load"].sum().to_list())
+gl1 = np.matrix(GL.loc[(GL["Quarter"]=="Q2") & (GL["Instance"] >= 48)]["Load"].tolist())
+s1 = np.matrix(S.loc[(S["Quarter"]=="Q2") & (S["Instance"] >= 48)]["Generation"].tolist())
+w1 = np.matrix(W.loc[(W["Quarter"]=="Q2") & (W["Instance"] >= 48)]["Generation"].tolist())
 
-def OPT1(ES,EW,EL,HL,cs=10000,cw=1000000,ch=10,chte=0,fhte=1,mhte=10,ceth=0,feth=1,meth=10):
+# %%
+
+def OPT1(ES,EW,EL,HL,cs=10000,cw=1000000,ch=10000,chte=0,fhte=1,mhte=10,ceth=0,feth=1,meth=10):
     #cost of solar panels
     #cost of wind turbines
     #cost of hydrogen storage???
@@ -102,4 +110,10 @@ def OPT1(ES,EW,EL,HL,cs=10000,cw=1000000,ch=10,chte=0,fhte=1,mhte=10,ceth=0,feth
     string = "Total cost: {} \nPanels: {} \nTurbines: {} \nH2 needed capacity: {}".format(model.ObjVal,ns.X,nw.X,nh.X)
     return print(string)
 
+
+# %%
+
+fig, ax = plt.subplots(1,1)
+x=np.linspace(0,24,96)
+plt.plot(x,el[0,:].transpose()+gl[0,:].transpose(),x,100000*s[0].transpose(),x,200*w[0].transpose())
 
