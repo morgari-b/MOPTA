31418487acb2c71d64d05d1bf601f3f7a28788dc
frulcino <ghjuber@gmail.com>
2024-05-04 16:48:09 +0200

    started scenairo generation modeling

--- fetch_data.py
+++ fetch_data.py
@@ -6,10 +6,13 @@ Created on Fri May  3 16:41:40 2024
 @author: frulcino
 """
 
+# %% import
 from entsoe import EntsoePandasClient
 import pandas as pd
-
-
+import matplotlib.pyplot as plt
+from datetime import timedelta
+import numpy as np
+# %% define functions
 def load_api_key(filepath):
     with open(filepath, 'r') as file:
         api_key = file.readline().strip()
@@ -18,17 +21,82 @@ def load_api_key(filepath):
 def select_energy_types(df, energy_type_list):
     columns = [col for col in df.columns if col[1] in energy_type_list]
     return df[columns]
-api_key_path = '../APIkeys/entsoe.txt'
 
+
+def fetch_data(start, end, country_code, api_key, max_retries = 3):
+    client = EntsoePandasClient(api_key=api_key)
+    # Set the start and end date
+    start_date = pd.Timestamp(f'{start}', tz='Europe/Brussels')
+    end_date = pd.Timestamp(f'{end}', tz='Europe/Brussels')
+    
+    #def fetch_data_instance(country_code, start=start_date, end=current_end_date):
+    # Create an empty DataFrame to store the aggregated data
+    full_generation_data = pd.DataFrame()
+
+    # Loop over the date range in steps of 5 days
+    while start_date < end_date:
+        # Calculate the end of the current 5-day period
+        current_end_date = start_date + timedelta(days=4)
+        if current_end_date > end_date:
+            current_end_date = end_date
+
+        try:
+            # Fetch generation data for the current period
+            print(f"Fetching data from {start_date} to {current_end_date}")
+            generation = client.query_generation_per_plant(country_code, start=start_date, end=current_end_date)
+            full_generation_data = pd.concat([full_generation_data, generation])
+        except Exception as e:
+            print(f"Error fetching datam retrying: {e}")
+            for i in np.arange(max_retries):
+                try:
+                    # Fetch generation data for the current period
+                    print(f"Fetching data from {start_date} to {current_end_date}")
+                    generation = client.query_generation_per_plant(country_code, start=start_date, end=current_end_date)
+                    full_generation_data = pd.concat([full_generation_data, generation])
+                    break
+                except Exception as e:
+                    print(f"Error fetching data for {i+2} time: {e}")
+        finally:
+            # Increment start_date to the next interval
+            start_date = current_end_date + timedelta(days=1)
+
+    return full_generation_data
+
+
+# The script now tries to fetch the data, and no matter the outcome, it attempts to save what has been fetched.
+
+
+# %% load key
+api_key_path = '../APIkeys/entsoe.txt'
 api_key = load_api_key(api_key_path)
 client = EntsoePandasClient(api_key=api_key)
 
-start = pd.Timestamp('20220101', tz='Europe/Brussels')
-end = pd.Timestamp('20220102', tz='Europe/Brussels')
+
+# %% set parameters 
+start = pd.Timestamp('20220301', tz='Europe/Brussels')
+end = pd.Timestamp('20220306', tz='Europe/Brussels')
 country_code = 'BE'  # Belgium ISO code
-energy_type_list = ["Wind Offshore"]
+energy_type_list = ["Wind Offshore", "Wind Onshore"]
+average_wind_generation = 4*2.539914194598637 #hour wind generation for 1h intervals
+# %% fetch data
 # Fetch generation per plant
-generation = client.query_generation_per_plant(country_code, start=start, end=end)
+#generation = client.query_generation_per_plant(country_code, start=start, end=end)
 
+try:
+    generation_data = fetch_data(start, end, country_code, api_key)
+finally:
+    # Save the data to CSV regardless of whether the fetch was completely successful
+    filename = f'generation_data_{country_code}_{2022}_to_{2023}.csv'
+    generation_data.to_csv(filename, index=False)
+    print(f"Data saved to {filename}")
 
+#generation = select_energy_types(generation, energy_type_list)
+# %% data manipulation
 
+#generation.columns = [col[0].split(" ")[0] + col[0].split(" ")[-1] for col in generation.columns] #rename columns
+#n_gens = (generation.mean()/average_wind_generation).astype(int) #get n_gens for each plant
+#generation = generation / n_gens #transform to per unit MW production
+# %%
+#generation.plot()
+
+# %% save data
---
+++ model_distribution.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from math import erf, sqrt
+
+def phi(x, mu, var):
+    """
+    Cumulative distribution function for normal distribution.
+    x: real value
+    mu: mean
+    var: variance
+    """
+    return (1 + erf((x - mu) / sqrt(2 * var))) / 2.0
+
+# Generate random dataset.
+mean = np.linspace(30, 50, 24)
+A = np.random.random((24, 24))
+covar = np.dot(A, A.transpose())  # Ensure covariance matrix is symmetric and positive semi-definite
+
+# Plot setup
+fig, axes = plt.subplots(2, 3, figsize=(15, 10))  # Subplots in a 2x3 grid
+fig.suptitle("Copula multivariate estimation overview")
+
+
+c = axes[0,0].imshow(covar, interpolation='nearest')
+fig.colorbar(c, ax=axes[0,0])
+axes[0,0].set_title("Multivariate Gaussian Covariance Matrix")
+
+
+# Create random multivariate normal data
+Y = np.random.multivariate_normal(mean, covar, 1000)
+time_range = pd.date_range(start="20210101", end="20210102", periods=24)
+Y = pd.DataFrame(Y, columns=time_range)
+
+
+# Plot the first variable's histogram
+axes[0, 1].hist(Y.iloc[:, 0], bins=20, color='skyblue', edgecolor='black')
+axes[0, 1].set_title("Marginal distribution of var0")
+
+# Parametric estimation of marginal distributions
+mean_est = Y.mean(axis=0)
+scale_est = np.sqrt(Y.var(axis=0, ddof=1))
+
+### random checks
+
+#uniform variables estimation
+X_h = pd.DataFrame(index=Y.index, columns=Y.columns)
+O_h = pd.DataFrame(index=Y.index, columns=Y.columns)
+for var in Y.columns:
+    X_h[var] = norm.cdf(Y[var], loc=mean_est[var], scale=scale_est[var])
+    O_h[var] = norm.ppf(X_h[var])
+# Histogram of transformed data to uniform distribution using estimated CDF
+axes[0, 2].hist(X_h.iloc[:, 0], bins=20, color='lightgreen', edgecolor='black')
+axes[0, 2].set_title("Uniform distrib by composing data via estimated CDF") #se non sembra uniforma not a good estimation
+
+axes[1, 0].hist(O_h.iloc[:, 0], bins=20, color='lightgreen', edgecolor='black')
+axes[1, 0].set_title("this should be gaussian") #se non sembra uniforma not a good estimation
+
+#copula covariance estimation:
+E_h = np.cov(O_h.T, ddof = 1)
+
+#plot
+#c = axes[1,1].imshow(E_h)
+#fig.colorbar(c, ax = axes[1,1])
+#axes[1,1].set_title("copula Covariance Matrix")
+   
+
+### GENERATE SCENARIOS
+#copula_scenarios
+n_scenarios = 1000
+n_vars = len(Y.columns)
+X_scen = pd.DataFrame(np.random.multivariate_normal(np.array([0]*n_vars), E_h, n_scenarios), columns = Y.columns)
+#convert to uniform domain U and the to inidital domain Y
+U_scen = pd.DataFrame( columns=Y.columns)
+scenarios = pd.DataFrame( columns=Y.columns) #scenario dataframe
+
+for var in Y.columns:
+    U_scen[var] = norm.cdf(X_scen[var])
+    scenarios[var] = norm.ppf(U_scen[var], loc = mean_est[var], scale = scale_est[var])
+
+
+# Plot the first variable's histogram
+axes[1, 1].hist(scenarios.iloc[:, 0], bins=20, color='skyblue', edgecolor='black')
+axes[1, 1].set_title("approx Marginal distribution of var0")
+
+scenarios_covariance = np.cov(scenarios.T, ddof = 1)
+c = axes[1,2].imshow(scenarios_covariance)
+fig.colorbar(c, ax = axes[1,2])
+axes[1,2].set_title("scenarios_covariance matrix")
+plt.tight_layout()
+plt.show()
