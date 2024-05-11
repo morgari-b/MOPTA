8dc92211ca138a3db9556fd26763868d8f55af1c
frulcino <ghjuber@gmail.com>
2024-05-11 22:20:02 +0200

    added PV SG

--- scenario_generation.py
+++ scenario_generation.py
@@ -16,6 +16,29 @@ from scipy import stats
 from reliability.Fitters import Fit_Weibull_2P
 import os
 os.chdir('/home/frulcino/codes/MOPTA/')
+
+
+#TODOS
+# - Can I use Y instead of O_h directly?
+# %% helper functions
+
+def date_format(df, country):
+    grouped = df[country].groupby(lambda date: (date.dayofyear, date.hour), axis = 0)
+    ran = False
+    n_instances = len(grouped)
+    X = {}
+    for time_instance, time_instance_df in grouped:
+        time_instance_df.index = time_instance_df.index.year
+        X[time_instance] = time_instance_df          
+    X = pd.DataFrame(X).sort_index()
+    X.columns.names = "day", "hour"
+    
+    #drop last day:
+    X = X.loc[:,:(365,23)]
+    return X
+      #print(time_instance, time_instance_df)
+      #  
+# %% wind fit
 def fit_weibull(Y, method = "LS"):
     """
     Y : pandas dataframe having as columns variables and as rows scenarios
@@ -29,6 +52,8 @@ def fit_weibull(Y, method = "LS"):
         
     return parameters_df
 
+
+    
 def fit_multivariate_weib(Y, method = "LS"):
     """
     Fit data with multivariate distribution having Weibull marginals coupled with Gaussian copula.
@@ -159,46 +184,141 @@ def SG_weib_example(n_samples = 100, n_scenarios = 100, n_vars = 24, shape = 2, 
 
     plt.show()
   
-def date_format(df, country):
-  grouped = df[country].groupby(lambda date: (date.dayofyear, date.hour), axis = 0)
-  ran = False
-  n_instances = len(grouped)
-  X = {}
-  for time_instance, time_instance_df in grouped:
-      time_instance_df.index = time_instance_df.index.year
-      X[time_instance] = time_instance_df          
-  X = pd.DataFrame(X).sort_index()
-  X.columns.names = "day", "hour"
+# %% solar fit
+def betapar_alpha(mean, var):
+    return mean*((mean*(1-mean)/var)-1)
+
+def betapar_beta(mean,var):
+    return (1-mean)*((mean*(1-mean)/var)-1)
+
+def fit_beta(Y):
+    """
+    Y : pandas dataframe having as columns variables and as rows scenarios
+    returns dataframe having estimated with Moments estimator, look at stackexchange for explanation
+    """
+    
+    night_hours = list(Y.iloc[:,list(Y.mean() <= 0.05)].columns)
+    Y = Y.iloc[:,list(Y.mean() > 0.05)]
+    parameters_df = pd.DataFrame(columns = Y.columns)
+    
+    for var in Y.columns:
+        Y_var = Y[var]
+        Y_mean = Y_var.mean()
+        Y_var = Y_var.var()
+        #params = stats.beta.fit(Y_var[Y_var >= 0.01], floc = 0, fscale = 1)
+        parameters_df.loc["zero_weight", var] = 0 #probability of solar production to be zero
+        alpha, beta =  betapar_alpha(Y_mean, Y_var), betapar_beta(Y_mean, Y_var)
+        parameters_df.loc["alpha", var],parameters_df.loc["beta", var] = alpha, beta
+        
+    return parameters_df, night_hours
+
+def fit_multivariate_beta(Y):
+    """
+    Fit data with multivariate distribution having Weibull marginals coupled with Gaussian copula.
+    input
+        Y: Pandas dataframe taking having as columns variables and rows as samples
+    output
+        parameters_df: df having for each variable the estimated parameters of the Weibull distribution
+        E_h: the estimated covariance matrix of the Gaussian Copula
+        
+    """
+    #def sbeta_cdf(zero_weight, alpha, beta):
+    #    """
+    #    beta cdf with a non zero weight at zero
+    #    """
+    #weibul distribution estimation
+    parameters_df, night_hours = fit_beta(Y)
+
+    Y = Y.iloc[:,list(Y.mean() > 0.05)]
+    X_h = pd.DataFrame(index=Y.index, columns=Y.columns)
+    O_h = pd.DataFrame(index=Y.index, columns=Y.columns)
+    for var in Y.columns:
+        #bring data to uniform domain
+        alpha, beta = parameters_df.loc["alpha", var], parameters_df.loc["beta", var]
+        X_h[var] = stats.beta.cdf(Y[var], alpha, beta)
+        O_h[var] = stats.beta.ppf(X_h[var], alpha, beta)
+        
+    #copula covariance estimation:
+    E_h = np.cov(O_h.T, ddof = 1) #non posso usare Y_h direttamente?
+    
+
+    c= plt.imshow(E_h, interpolation = "nearest")
+    plt.colorbar(c)
+    plt.title("Gaussian copula covariance matrix")
   
-  #drop last day:
-  X = X.loc[:,:(365,23)]
-  return X
-      #print(time_instance, time_instance_df)
-      #  
+    return parameters_df, E_h, night_hours
+#%%
+def SG_beta(n_scenarios, var_names, parameters_df, E_h, night_hours, save = True, filename = "PV_scenario"):
+        """
+        Parameters
+        ----------
+        n_scenarios : integer
+            number of scenarios to generate
+        parameters_df : panda DataFrame
+            having as columns the variables and as rows "scale" and "shape" parameters of 
+            the Weibull marginal distributions
+        E_h : np.array n_var x n_var
+            estimated covariance matrix of the Gaussian copula
+        
+        Returns
+        -------
+        scenarios: panda DataFrame
+            each row is a scenario
+        
+        """
+        n_vars = len(var_names)
+        X_scen = pd.DataFrame(np.random.multivariate_normal(np.array([0]*n_vars), E_h, n_scenarios), columns = var_names)
+        #convert to uniform domain U and the to initial domain Y
+        U_scen = pd.DataFrame( columns=var_names)
+        scenarios = pd.DataFrame( columns=var_names) #scenario dataframe
+        night_scenarios = pd.DataFrame(np.zeros((n_scenarios, len(night_hours))), columns=night_hours)
+        for var in var_names:
+            U_scen[var] = stats.norm.cdf(X_scen[var])
+            alpha, beta = parameters_df.loc["alpha",var], parameters_df.loc["beta",var]
+            scenarios[var] = stats.beta.ppf(U_scen[var], alpha, beta)
+
+        scenarios = pd.concat([scenarios, night_scenarios], axis = 1)
+        scenarios = scenarios.reindex(sorted(scenarios.columns), axis = 1)
+        n_hours = len(scenarios.columns)
+        scenarios.columns = pd.date_range("01/01/2023", periods = n_hours, freq="h")
+        if save:
+            scenarios.to_csv("scenarios/"+filename+f"{n_scenarios}")
+        return scenarios
 #%% fetch data
 
 
-wind_pu = pd.read_csv("WindNinja/renewables_ninja_europe_wind_output_1_current.csv", index_col = 0)
-PV_pu = pd.read_csv("PVNinja/ninja_pv_europe_v1.1_merra2.csv", index_col = 0)
+wind_pu = pd.read_csv("../WindNinja/renewables_ninja_europe_wind_output_1_current.csv", index_col = 0)
+PV_pu = pd.read_csv("../PVNinja/ninja_pv_europe_v1.1_merra2.csv", index_col = 0)
 wind_pu.index = pd.to_datetime(wind_pu.index, format="%d/%m/%Y %H:%M")
 PV_pu.index = pd.to_datetime(PV_pu.index, format = "%Y-%m-%d %H:%M:%S")
 
 # %% format data for country
-countries = wind_pu.columns
-country = countries[0]
+#countries = wind_pu.columns
+#country = countries[0]
 
-X = date_format(wind_pu, country)
+#X = date_format(wind_pu, country)
 
 
 #params_dict = {} #for each country
 #%% fit
-params = fit_multivariate_weib(X)
+#params = fit_multivariate_weib(X)
 
 #%%
-E = params[1]
-E_zoom = E[0:24*7,0:24*7]
-c= plt.imshow(E_zoom, interpolation = "nearest")
-plt.colorbar(c)
-plt.title("Gaussian copula covariance matrix")
+#E = params[1]
+#E_zoom = E[0:24*7,0:24*7]
+#c= plt.imshow(E_zoom, interpolation = "nearest")
+#plt.colorbar(c)
+#plt.title("Gaussian copula covariance matrix")
+
+# %%
+X_PV = date_format(PV_pu,"AL")
+#%%
+
+parameters_df, E_h, night_hours = fit_multivariate_beta(X_PV)
+#%%
+n_scenarios = 10
+var_names = parameters_df.columns
+
+
 
 
