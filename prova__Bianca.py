075311314f57a2dfdb00b6ac33e406db41883fa5
frulcino <ghjuber@gmail.com>
2024-05-13 18:23:13 +0200

    added main in prova Bianca

--- prova__Bianca.py
+++ prova__Bianca.py
@@ -35,20 +35,23 @@ w = np.matrix(w)
 
 # %%
 def OPT(ES,EW,EL,HL,cs=4000,cw=3000000,Mns=np.inf,Mnw=100,Mnh=np.inf,ch=7,chte=0,fhte=0.75,Mhte=200000,ceth=0,feth=0.7,Meth=15000):
-    #cost of solar panels
-    #cost of wind turbines
-    #max socially acceptable wind turbines
-    #cost of hydrogen storage???
-    #cost of H to el
-    #efficiency of H to el
-    #max H to el at an instance
-    #cost of el to H
-    #efficiency of el to H
-    #max el to H at an instance
-    #solar el production - dx96 array
-    #wind el production - dx96 array
-    #electricity load - dx96 array
-    #hydrogen load - dx96 array
+    """
+    ES: solar el production - dxinst array
+    EW: wind el production - dxinst array
+    EL: electricity load - dxinst array
+    HL: hydrogen load - dxinst array
+    cs: cost of solar panels
+    c2: cost of wind turbines
+    mw: max socially acceptable wind turbines
+    ch: cost of hydrogen storage???
+    chte: cost of H to el
+    fhte: efficiency of H to el
+    Mhte: max H to el at an instance
+    ceth: cost of el to H
+    feth: efficiency of el to H
+    Meth: max el to H at an instance
+    
+    """
     
     start_time=time.time()
     
@@ -285,133 +288,134 @@ w1 = np.matrix(w1)
 #H2 needed capacity: 27527.186905388273
 
 # %%
-
-# Example of 4 consecutive days: decreasing generation, same consumption
-
-el1=[]
-gl1=[]
-s1=[]
-w1=[]
-for i in range(4):
-    el1 = el1 + EL.loc[EL["Quarter"]=="Q{}".format(i+1)].groupby("Instance")["Load"].sum().to_list()
-    gl1 = gl1 + GL.loc[GL["Quarter"]=="Q{}".format(i+1)]["Load"].tolist()
-    s1 = s1 + ((3-i)/9*S.loc[S["Quarter"]=="Q{}".format(i+1)]["Generation"]).tolist()
-    w1 = w1 + ((3-i)/9*W.loc[W["Quarter"]=="Q{}".format(i+1)]["Generation"]).tolist()
-el1 = np.matrix(el1)
-gl1 = np.matrix(gl1)
-s1 = np.matrix(s1)
-w1 = np.matrix(w1)
-
-#OPT(s1,w1,el1,gl1)
-
-#Status: 2
-#Total cost: 7247484221.179818
-#Panels: 446436.0
-#Turbines: 50.0
-#H2 needed capacity: 531174.0221179818
-
-
-#%%
-
-# Suppose we have more than 4 consecutive days: aggregate data by hour
-
-el2=[]
-gl2=[]
-s2=[]
-w2=[]
-for i in range(4):
-    el2 = el2 + 4*EL.loc[EL["Quarter"]=="Q{}".format(i+1)].groupby((EL["Instance"]-1)//4)["Load"].sum().to_list()
-    gl2 = gl2 + 4*GL.loc[GL["Quarter"]=="Q{}".format(i+1)].groupby((GL["Instance"]-1)//4)["Load"].sum().to_list()
-    s2 = s2 + 4*S.loc[S["Quarter"]=="Q{}".format(i+1)].groupby((S["Instance"]-1)//4)["Generation"].sum().to_list()
-    # first attempt: wind is trated like others. second attempt: consider wind as it is
-    #w2 = w2 + 4*W.loc[W["Quarter"]=="Q{}".format(i+1)].groupby((W["Instance"]-1)//4)["Generation"].sum().to_list()
-    w2 = w2 + W.loc[W["Quarter"]=="Q{}".format(i+1)]["Generation"].to_list()
-el2 = np.matrix(el2)
-gl2 = np.matrix(gl2)
-s2 = np.matrix(s2)
-w2 = np.matrix(w2)
-
-fig, ax = plt.subplots(1,1)
-x=np.linspace(0,1,384)
-plt.plot(x,el2.transpose(),"yellow",x,0.033*gl2.transpose(),"green",x,38173*s2.transpose(),"orange",x,50*w2.transpose(),"blue")
-
-
-#OPT(s2,w2,el2,gl2)
-
-#Status: 2
-#Total cost: 568402068.2360864
-#Panels: 38173.0
-#Turbines: 50.0
-#H2 needed capacity: 26571.000063605356
-#Max EtH: 481.16767793471075
-#Max HtE: 6278.835611784831
-
-# so.... aggregating by hour doesn't really change much (compare with first example)
-# most variability is between seasons and day/night
-# but if I treat wind as variable problem becomes unfeasable within contraints
-# aka if I don't have wind for two days I'm soup
-
-#%%
-
-el3=[]
-gl3=[]
-s3=[]
-w3=[]
-for i in range(4):
-    el3 = el3 + EL.loc[EL["Quarter"]=="Q{}".format(i+1)].groupby((EL["Instance"]-1)//4)["Load"].sum().to_list()
-    gl3 = gl3 + GL.loc[GL["Quarter"]=="Q{}".format(i+1)].groupby((GL["Instance"]-1)//4)["Load"].sum().to_list()
-    s3 = s3 + S.loc[S["Quarter"]=="Q{}".format(i+1)].groupby((S["Instance"]-1)//4)["Generation"].sum().to_list()
-    #w3 = w3 + W.loc[W["Quarter"]=="Q{}".format(i+1)].groupby((W["Instance"]-1)//4)["Generation"].sum().to_list()
-w3 = W.loc[W["Quarter"]=="Q1"]["Generation"]
-el3 = np.matrix(el3)
-gl3 = np.matrix(gl3)
-s3 = np.matrix(s3)
-w3 = np.matrix(w3)
-
-#fig, ax = plt.subplots(1,1)
-#x=np.linspace(0,1,96)
-#plt.plot(x,el3.transpose(),"yellow",x,0.033*gl3.transpose(),"green",x,38173*s3.transpose(),"orange",x,50*w3.transpose(),"blue")
-
-costs=[]
-for mw in [50,75,100,150,200]:
-    costs += [OPT(s3,w3,el3,gl3,mw=mw)]
-print(costs)
-plt.plot([50,75,100,150,200], costs)
-
-#%%
-Ws = W.sort_values(by="Generation").reset_index()["Generation"].to_list()
-
-# observation: wind is modelled through weibull distribution. Look for realistic parameters
-Ys = np.sort(2.9*np.random.weibull(3.5,384))
-#Ys = [4/(1+math.exp(-x+2)) for x in Ys]  # model using sigmoid activation function
-Ys[:] = [4 if x>4 else x for x in Ys]
-
-Xs = np.linspace(0,1,384)
-plt.plot(Xs,Ys,"blue",Xs,Ws,"green")
-
-#%% QUESTION: Seasonal variance?
-Ys = np.sort(2.9*np.random.weibull(3.5,96))
-#Ys = [5/(1+math.exp(-x+3)) for x in Ys]
-Ys[:] = [4 if x>4 else x for x in Ys]
-
-plt.plot(np.linspace(0,24,96), np.sort(w[0]).transpose(),"blue",
-         np.linspace(0,24,96), np.sort(w[1]).transpose(),"green",
-         np.linspace(0,24,96), np.sort(w[2]).transpose(),"yellow",
-         np.linspace(0,24,96), np.sort(w[3]).transpose(),"brown",
-         
-         np.linspace(0,24,96), Ys,"red")
-
-#%%
-for i in np.linspace(4,9,4):
-    plt.plot(np.linspace(0,24,96), np.sort(np.random.weibull(i,96)))
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    
+    # Example of 4 consecutive days: decreasing generation, same consumption
+    
+    el1=[]
+    gl1=[]
+    s1=[]
+    w1=[]
+    for i in range(4):
+        el1 = el1 + EL.loc[EL["Quarter"]=="Q{}".format(i+1)].groupby("Instance")["Load"].sum().to_list()
+        gl1 = gl1 + GL.loc[GL["Quarter"]=="Q{}".format(i+1)]["Load"].tolist()
+        s1 = s1 + ((3-i)/9*S.loc[S["Quarter"]=="Q{}".format(i+1)]["Generation"]).tolist()
+        w1 = w1 + ((3-i)/9*W.loc[W["Quarter"]=="Q{}".format(i+1)]["Generation"]).tolist()
+    el1 = np.matrix(el1)
+    gl1 = np.matrix(gl1)
+    s1 = np.matrix(s1)
+    w1 = np.matrix(w1)
+    
+    #OPT(s1,w1,el1,gl1)
+    
+    #Status: 2
+    #Total cost: 7247484221.179818
+    #Panels: 446436.0
+    #Turbines: 50.0
+    #H2 needed capacity: 531174.0221179818
+    
+    
+    #%%
+    
+    # Suppose we have more than 4 consecutive days: aggregate data by hour
+    
+    el2=[]
+    gl2=[]
+    s2=[]
+    w2=[]
+    for i in range(4):
+        el2 = el2 + 4*EL.loc[EL["Quarter"]=="Q{}".format(i+1)].groupby((EL["Instance"]-1)//4)["Load"].sum().to_list()
+        gl2 = gl2 + 4*GL.loc[GL["Quarter"]=="Q{}".format(i+1)].groupby((GL["Instance"]-1)//4)["Load"].sum().to_list()
+        s2 = s2 + 4*S.loc[S["Quarter"]=="Q{}".format(i+1)].groupby((S["Instance"]-1)//4)["Generation"].sum().to_list()
+        # first attempt: wind is trated like others. second attempt: consider wind as it is
+        #w2 = w2 + 4*W.loc[W["Quarter"]=="Q{}".format(i+1)].groupby((W["Instance"]-1)//4)["Generation"].sum().to_list()
+        w2 = w2 + W.loc[W["Quarter"]=="Q{}".format(i+1)]["Generation"].to_list()
+    el2 = np.matrix(el2)
+    gl2 = np.matrix(gl2)
+    s2 = np.matrix(s2)
+    w2 = np.matrix(w2)
+    
+    fig, ax = plt.subplots(1,1)
+    x=np.linspace(0,1,384)
+    plt.plot(x,el2.transpose(),"yellow",x,0.033*gl2.transpose(),"green",x,38173*s2.transpose(),"orange",x,50*w2.transpose(),"blue")
+    
+    
+    #OPT(s2,w2,el2,gl2)
+    
+    #Status: 2
+    #Total cost: 568402068.2360864
+    #Panels: 38173.0
+    #Turbines: 50.0
+    #H2 needed capacity: 26571.000063605356
+    #Max EtH: 481.16767793471075
+    #Max HtE: 6278.835611784831
+    
+    # so.... aggregating by hour doesn't really change much (compare with first example)
+    # most variability is between seasons and day/night
+    # but if I treat wind as variable problem becomes unfeasable within contraints
+    # aka if I don't have wind for two days I'm soup
+    
+    #%%
+    
+    el3=[]
+    gl3=[]
+    s3=[]
+    w3=[]
+    for i in range(4):
+        el3 = el3 + EL.loc[EL["Quarter"]=="Q{}".format(i+1)].groupby((EL["Instance"]-1)//4)["Load"].sum().to_list()
+        gl3 = gl3 + GL.loc[GL["Quarter"]=="Q{}".format(i+1)].groupby((GL["Instance"]-1)//4)["Load"].sum().to_list()
+        s3 = s3 + S.loc[S["Quarter"]=="Q{}".format(i+1)].groupby((S["Instance"]-1)//4)["Generation"].sum().to_list()
+        #w3 = w3 + W.loc[W["Quarter"]=="Q{}".format(i+1)].groupby((W["Instance"]-1)//4)["Generation"].sum().to_list()
+    w3 = W.loc[W["Quarter"]=="Q1"]["Generation"]
+    el3 = np.matrix(el3)
+    gl3 = np.matrix(gl3)
+    s3 = np.matrix(s3)
+    w3 = np.matrix(w3)
+    
+    #fig, ax = plt.subplots(1,1)
+    #x=np.linspace(0,1,96)
+    #plt.plot(x,el3.transpose(),"yellow",x,0.033*gl3.transpose(),"green",x,38173*s3.transpose(),"orange",x,50*w3.transpose(),"blue")
+    
+    costs=[]
+    for mw in [50,75,100,150,200]:
+        costs += [OPT(s3,w3,el3,gl3,mw=mw)]
+    print(costs)
+    plt.plot([50,75,100,150,200], costs)
+    
+    #%%
+    Ws = W.sort_values(by="Generation").reset_index()["Generation"].to_list()
+    
+    # observation: wind is modelled through weibull distribution. Look for realistic parameters
+    Ys = np.sort(2.9*np.random.weibull(3.5,384))
+    #Ys = [4/(1+math.exp(-x+2)) for x in Ys]  # model using sigmoid activation function
+    Ys[:] = [4 if x>4 else x for x in Ys]
+    
+    Xs = np.linspace(0,1,384)
+    plt.plot(Xs,Ys,"blue",Xs,Ws,"green")
+    
+    #%% QUESTION: Seasonal variance?
+    Ys = np.sort(2.9*np.random.weibull(3.5,96))
+    #Ys = [5/(1+math.exp(-x+3)) for x in Ys]
+    Ys[:] = [4 if x>4 else x for x in Ys]
+    
+    plt.plot(np.linspace(0,24,96), np.sort(w[0]).transpose(),"blue",
+             np.linspace(0,24,96), np.sort(w[1]).transpose(),"green",
+             np.linspace(0,24,96), np.sort(w[2]).transpose(),"yellow",
+             np.linspace(0,24,96), np.sort(w[3]).transpose(),"brown",
+             
+             np.linspace(0,24,96), Ys,"red")
+    
+    #%%
+    for i in np.linspace(4,9,4):
+        plt.plot(np.linspace(0,24,96), np.sort(np.random.weibull(i,96)))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- test1gui.py
+++ test1gui.py
@@ -14,11 +14,189 @@ import pandas as pd
 import numpy as np
 
 #import our functions:
-from scenario_generation import read_parameters, SG_beta, SG_weib
+from scenario_generation import read_parameters, SG_beta, SG_weib, quarters_df_to_year
 from opt import run_OPT
 
 
 # %%
+"""
+TODOS:
+    - Generalize scenario generation:
+        - More years
+        - Grouped 
+        - Option for running on only Challenge Days
+"""
+
+# Worker class that handles the long-running task
+class ScenarioGenerator(QObject):
+    finished = pyqtSignal()  # Signal to indicate completion
+    progress = pyqtSignal(str)  # Signal to send progress text back to GUI
+    results_ready = pyqtSignal(object)
+    
+    def __init__(self, n_scenarios, country, params):
+        super().__init__()
+        self.n_scenarios = n_scenarios
+        self.country = country
+        self.params = params
+        self.result = None #read only after SG
+        
+
+    def run(self):
+        self.progress.emit(f"Generating Wind scenarios for {self.country}...")
+        # Generate Scenarios
+        n_scenarios = self.n_scenarios
+        params = self.params
+        scenarios = {}
+        scenarios["wind"] = SG_weib(n_scenarios, params["wind"][0], params["wind"][1])
+        self.progress.emit(f"Wind done, generating PV scenarios for {self.country}...")
+        scenarios["PV"] = SG_beta(n_scenarios, parameters_df = params["PV"][0], E_h = params["PV"][1], night_hours = params["PV"][2], save = False)
+        # After processing
+        self.progress.emit("Scenarios generated, fetching Electricity loads...")
+        #Fetch loads: 
+        path = "data.xlsx"
+        EL=pd.read_excel(path,sheet_name='Electricity Load')
+        GL=pd.read_excel(path,sheet_name='Gas Load')
+        locations = EL["Location_Electricity"].unique()
+        scenarios["Electricity_load"] = {}
+        scenarios["Hydrogen_load"] = {}
+        for location in locations:
+            #loads are provided by one single day for season, for each location, we expand the days to each day in the season for all year
+            scenarios["Electricity_load"][location] = quarters_df_to_year("electric-demand", location, EL, "Load", "Location_Electricity", save = False, n_scenarios = n_scenarios, n_hours = 24*365)
+        #%%
+        self.progress.emit("Scenarios generated, fetching Hydrogen loads...")
+        scenarios["Hydrogen_load"]["g"] = quarters_df_to_year("hydrogen-demand", "g", GL, "Load", "Location_Gas", save = False, n_scenarios = n_scenarios, n_hours = 24*365)
+        self.result = scenarios
+        self.results_ready.emit(self.result)
+        self.progress.emit("Done.")
+        self.finished.emit()  # Emit finished signal when done
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        
+        #initialize parameters
+        self.scenarios = None
+        
+        #App layout
+        super().__init__()
+        self.setWindowTitle("Hydrogen Network Optimization")
+        self.setGeometry(100, 100, 400, 600)
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
+
+        form_layout = QFormLayout()
+        self.location_input = QComboBox()
+        self.location_input.addItems(['Austria',
+                                     'Belgium',
+                                     'Bulgaria',
+                                     'Cyprus',
+                                     'Czech Republic',
+                                     'Germany',
+                                     'Denmark',
+                                     'Estonia',
+                                     'Spain',
+                                     'Finland',
+                                     'France',
+                                     'Greece',
+                                     'Croatia',
+                                     'Hungary',
+                                     'Italy',
+                                     'Lithuania',
+                                     'Luxembourg',
+                                     'Latvia',
+                                     'Malta',
+                                     'Netherlands',
+                                     'Poland',
+                                     'Portugal',
+                                     'Romania',
+                                     'Sweden',
+                                     'Slovenia',
+                                     'Slovakia'])  # Countries
+        form_layout.addRow("Geographical Location:", self.location_input)
+
+        self.n_scenarios_input = QLineEdit()
+        form_layout.addRow("Number of Scenarios:", self.n_scenarios_input)
+
+        self.layout.addLayout(form_layout)
+
+        self.SG_button = QPushButton("Generate Scenarios")
+        self.SG_button.clicked.connect(self.start_scenario_generation)
+        self.layout.addWidget(self.SG_button)
+
+        self.SG_output_label = QLabel("Output will be shown here.")
+        self.layout.addWidget(self.SG_output_label)
+
+        self.SG_canvas = FigureCanvas(Figure(figsize=(5, 5)))
+        self.layout.addWidget(self.SG_canvas)
+        
+        # Optimization Button
+        self.submit_button = QPushButton("Optimize Network")
+        self.submit_button.clicked.connect(self.run_optimization)
+        self.layout.addWidget(self.submit_button)
+        
+        # Optimization Output Area
+        self.canvas = FigureCanvas(Figure(figsize=(5, 5), dpi=100))
+        self.output_label = QLabel("Output will be shown here.")
+        self.output_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.layout.addWidget(self.output_label)
+        self.layout.addWidget(self.canvas)
+        
+        
+        
+    def start_scenario_generation(self):
+        n_scenarios = int(self.n_scenarios_input.text() or 1)
+        country = self.location_input.currentText()
+        params = read_parameters(country)
+        self.SG_output_label.setText("Parameters have been imported, generating Wind scenarios, this can take a while...")
+        
+        self.thread = QThread()  # Create a QThread object
+        self.worker = ScenarioGenerator(n_scenarios, country, params)  # Create a worker object
+        self.worker.moveToThread(self.thread)  # Move worker to thread
+        self.thread.started.connect(self.worker.run)  # Start worker when thread starts
+        self.worker.results_ready.connect(self.handle_scenarios_result) #send results to handle_scenarios_results
+        self.worker.finished.connect(self.thread.quit)  # Quit thread when worker is done
+        self.worker.finished.connect(self.worker.deleteLater)  # Delete worker when done
+        self.thread.finished.connect(self.thread.deleteLater)  # Delete thread when done
+        self.worker.progress.connect(self.update_output_label)  # Connect progress signal to update the GUI
+        self.SG_output_label.setText("Done.")
+        self.thread.start()
+    
+    def run_optimization(self):
+        hs_cost = 7
+        #hs_cost = self.hydrogen_cost_input.text() #hydrogen storage cost
+        #if hs_cost != "":
+        #    hs_cost = float(hs_cost)
+        #else:
+        #    hs_cost = 10000
+        
+        scenarios = self.scenarios
+        if scenarios == None:
+            self.output_label.setText("Generate Scenarios before running optimization")
+        else:
+            # Call the optimization function
+            result, fig = run_OPT(ch=hs_cost) 
+            self.canvas.figure.clf()
+            self.canvas.figure = fig
+            self.canvas.draw()
+            
+            # Display the results
+            self.output_label.setText(str(result))
+
+    def update_output_label(self, text):
+        self.SG_output_label.setText(text)
+        
+    def handle_scenarios_result(self, result):
+        print("Handling scenarios result")
+        self.scenarios = result #save scenarios for later optimization
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
 
 """
 class MainWindow(QMainWindow):
@@ -145,140 +323,3 @@ if __name__ == "__main__":
     window.show()
     sys.exit(app.exec())
 """
-
-# Worker class that handles the long-running task
-class ScenarioGenerator(QObject):
-    finished = pyqtSignal()  # Signal to indicate completion
-    progress = pyqtSignal(str)  # Signal to send progress text back to GUI
-    
-    def __init__(self, n_scenarios, country, params):
-        super().__init__()
-        self.n_scenarios = n_scenarios
-        self.country = country
-        self.params = params
-
-    def run(self):
-        # Here, implement the scenario generation logic
-        self.progress.emit(f"Generating Wind scenarios for {self.country}...")
-        # Simulate processing
-        n_scenarios = self.n_scenarios
-        params = self.params
-        self.EW = SG_weib(n_scenarios, params["wind"][0], params["wind"][1])
-        self.progress.emit(f"Wind done, generating PV scenarios for {self.country}...")
-        self.ES = SG_beta(n_scenarios, parameters_df = params["PV"][0], E_h = params["PV"][1], night_hours = params["PV"][2], save = False)
-        # After processing
-        self.progress.emit("Scenarios generated.")
-        self.finished.emit()  # Emit finished signal when done
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Hydrogen Network Optimization")
-        self.setGeometry(100, 100, 400, 600)
-
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-        self.layout = QVBoxLayout(self.central_widget)
-
-        form_layout = QFormLayout()
-        self.location_input = QComboBox()
-        self.location_input.addItems(['Austria',
-                                     'Belgium',
-                                     'Bulgaria',
-                                     'Cyprus',
-                                     'Czech Republic',
-                                     'Germany',
-                                     'Denmark',
-                                     'Estonia',
-                                     'Spain',
-                                     'Finland',
-                                     'France',
-                                     'Greece',
-                                     'Croatia',
-                                     'Hungary',
-                                     'Italy',
-                                     'Lithuania',
-                                     'Luxembourg',
-                                     'Latvia',
-                                     'Malta',
-                                     'Netherlands',
-                                     'Poland',
-                                     'Portugal',
-                                     'Romania',
-                                     'Sweden',
-                                     'Slovenia',
-                                     'Slovakia'])  # Countries
-        form_layout.addRow("Geographical Location:", self.location_input)
-
-        self.n_scenarios_input = QLineEdit()
-        form_layout.addRow("Number of Scenarios:", self.n_scenarios_input)
-
-        self.layout.addLayout(form_layout)
-
-        self.SG_button = QPushButton("Generate Scenarios")
-        self.SG_button.clicked.connect(self.start_scenario_generation)
-        self.layout.addWidget(self.SG_button)
-
-        self.SG_output_label = QLabel("Output will be shown here.")
-        self.layout.addWidget(self.SG_output_label)
-
-        self.SG_canvas = FigureCanvas(Figure(figsize=(5, 5)))
-        self.layout.addWidget(self.SG_canvas)
-        
-        # Optimization Button
-        self.submit_button = QPushButton("Optimize Network")
-        self.submit_button.clicked.connect(self.run_optimization)
-        self.layout.addWidget(self.submit_button)
-        
-        # Optimization Output Area
-        self.canvas = FigureCanvas(Figure(figsize=(5, 5), dpi=100))
-        self.output_label = QLabel("Output will be shown here.")
-        self.output_label.setAlignment(Qt.AlignmentFlag.AlignTop)
-        self.layout.addWidget(self.output_label)
-        self.layout.addWidget(self.canvas)
-        
-        
-        
-    def start_scenario_generation(self):
-        n_scenarios = int(self.n_scenarios_input.text() or 1)
-        country = self.location_input.currentText()
-        params = read_parameters(country)
-        self.SG_output_label.setText("Parameters have been imported, generating Wind scenarios, this can take a while...")
-        
-        self.thread = QThread()  # Create a QThread object
-        self.worker = ScenarioGenerator(n_scenarios, country, params)  # Create a worker object
-        self.worker.moveToThread(self.thread)  # Move worker to thread
-        self.thread.started.connect(self.worker.run)  # Start worker when thread starts
-        self.worker.finished.connect(self.thread.quit)  # Quit thread when worker is done
-        self.worker.finished.connect(self.worker.deleteLater)  # Delete worker when done
-        self.thread.finished.connect(self.thread.deleteLater)  # Delete thread when done
-        self.worker.progress.connect(self.update_output_label)  # Connect progress signal to update the GUI
-        self.SG_output_label.setText("Done.")
-        self.thread.start()
-    
-    def run_optimization(self):
-        hs_cost = self.hydrogen_cost_input.text() #hydrogen storage cost
-        if hs_cost != "":
-            hs_cost = float(hs_cost)
-        else:
-            hs_cost = 10000
-            
-        
-        print(type(hs_cost), hs_cost, "yeee")
-        # Call the optimization function
-        result, fig = run_OPT(ch=hs_cost) 
-        self.canvas.figure.clf()
-        self.canvas.figure = fig
-        self.canvas.draw()
-        
-        # Display the results
-        self.output_label.setText(str(result))
-
-    def update_output_label(self, text):
-        self.SG_output_label.setText(text)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
