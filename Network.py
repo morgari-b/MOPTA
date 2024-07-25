68455bb70d49b7f6e879e2b47d2d25dbedd37c03
frulcino <ghjuber@gmail.com>
2024-07-25 18:42:20 +0200

    first kind of working model on the grid

--- Network.py
+++ Network.py
@@ -160,14 +160,14 @@ time_index = pd.date_range('2023-01-01 00:00:00', '2023-12-31 23:00:00', freq='H
 
 elec_load_scenario = xr.DataArray(
     np.expand_dims(elec_load_df[['IT', 'ES', 'AT', 'FR']].values, axis = 2), #add one dimension to correspond with scenarios
-    coords={'time': time_index, 'node': ['IT', 'ES', 'AT', 'FR'], 'scenario': [0]},
+    coords={'time': time_index, 'node': ['Italy', 'Spain', 'Austria', 'France'], 'scenario': [0]},
     dims=['time', 'node', 'scenario']
 )
 
 scenario = 0
-wind_scenario = import_generated_scenario('scenarios/wind_scenarios.csv',4,scenario, node_names= ['IT', 'ES', 'AT', 'FR'])
-pv_scenario = import_generated_scenario('scenarios/PV_scenario.csv',4, scenario, node_names= ['IT', 'ES', 'AT', 'FR'])
-hydrogen_demand_scenario = import_generated_scenario('scenarios/hydrogen_demandg.csv',4, scenario, node_names= ['IT', 'ES', 'AT', 'FR'])
+wind_scenario = import_generated_scenario('scenarios/wind_scenarios.csv',4,scenario, node_names= ['Italy', 'Spain', 'Austria', 'France'])
+pv_scenario = import_generated_scenario('scenarios/PV_scenario.csv',4, scenario, node_names=['Italy', 'Spain', 'Austria', 'France'])
+hydrogen_demand_scenario = import_generated_scenario('scenarios/hydrogen_demandg.csv',4, scenario, node_names=['Italy', 'Spain', 'Austria', 'France'])
 
 eu.genW_t = wind_scenario
 eu.genS_t = pv_scenario
@@ -230,7 +230,7 @@ def OPT2(Network,
     #todo perchè lo rimuoviamo questo?
     cons1=model.addConstrs(nh[k]>=0 for k in range(Nnodes) )#for j in range(d) for i in range(inst))
     #todo: sobstitute 30 with a parameter
-    cons2=model.addConstrs(- H[j,i+1,k] + H[j,i,k] + 30*Network.n['feth'].iloc[k]*EtH[j,i,k] - HtE[j,i,k] -  
+    cons2=model.addConstrs(  H[j,i,k] - H[j,i+1,k] + 30*Network.n['feth'].iloc[k]*EtH[j,i,k] - HtE[j,i,k] -  
                            quicksum(H_edge[j,i,l] for l in Network.edgesH.loc[Network.edgesH['start_node']==Network.n.index.to_list()[k]].index.to_list()) +
                            quicksum(H_edge[j,i,l] for l in Network.edgesH.loc[Network.edgesH['end_node']==Network.n.index.to_list()[k]].index.to_list())
                            ==0 for j in range(d) for i in range(inst-1) for k in range(Nnodes))
@@ -243,11 +243,7 @@ def OPT2(Network,
     for group in range(rounds):
         gr_start_time=time.time()
 
-        # PER GABOR!!!!
-        # così sto prendendo gli stati non gli scenari vanno messi i nodi.
-        # idea: cambia struttura di Network.loadE etc dipendenti dal tempo in modo da avere i paesi come colonna e un'altra colonna con "scenario"
-        # così da avere semplicemente più righe con gli scenari per ogni paese.
-        # aggiusta qui usando lo stesso trucchetto di sotto con ==.
+    
         ES = Network.genS_t.sel(scenario = slice(d*group, (d+1)*group))
         EW = Network.genW_t.sel(scenario = slice(d*group, (d+1)*group))
         EL = Network.loadE_t.sel(scenario = slice(d*group, (d+1)*group))
@@ -289,6 +285,122 @@ def OPT2(Network,
 # %%
 eu.costs.shape[0]
 outputs = OPT2(eu)
-# %%
 
+#%% Ho riscritto il modello in maniera un po' più canon
+def OPT3(Network,
+         d=1,rounds=1
+         ):
+    
+    if Network.costs.shape[0] == 1: #if the costs are the same:
+        cs, cw, ch, chte, ceth, cNTC, cMH = Network.costs['cs'][0], Network.costs['cw'][0], Network.costs['ch'][0], Network.costs['chte'][0], Network.costs['ceth'][0], Network.costs['cNTC'][0], Network.costs['cMH'][0]
+    else:
+        print("add else") #actually we can define the costs appropriately using the network class directly
+    
+
+    start_time=time.time()
+    
+    Nnodes = Network.n.shape[0]
+    NPedges = Network.edgesP.shape[0]
+    NHedges = Network.edgesH.shape[0]
+    D = eu.loadE_t.shape[2] #total number of scenarios
+    inst = Network.loadE_t.shape[0] #number of time steps T
+    rounds=min(rounds,D//d)
+
+    nodes = Network.n.index.to_list()
+    scenarios = range(d)
+    time_steps = range(inst)
+    Pedges = list(zip(eu.edgesP['start_node'].to_list(),eu.edgesP['end_node'].to_list()))
+    #print(Pedges.keys())
+    Hedges = list(zip(eu.edgesH['start_node'].to_list(),eu.edgesH['end_node'].to_list()))
+    print("\nSTARTING OPT2 -- setting up model for {} batches of {} scenarios.\n".format(rounds,d))
+    
+    env = Env(params={'OutputFlag': 0})
+    model = Model(env=env)
+    model.setParam('LPWarmStart',1)
+    #model.setParam('Method',1)
+    
+    ns = model.addVars(nodes,vtype=GRB.CONTINUOUS, obj=cs,ub=Network.n['Mns'])
+    nw = model.addVars(nodes,vtype=GRB.CONTINUOUS, obj=cw,ub=Network.n['Mnw'])    
+    nh = model.addVars(nodes,vtype=GRB.CONTINUOUS, obj=ch,ub=Network.n['Mnh'])   
+    mhte = model.addVars(nodes,vtype=GRB.CONTINUOUS,obj=0.01, ub=Network.n['Mhte'])
+    meth = model.addVars(nodes,vtype=GRB.CONTINUOUS,obj=0.01,ub=Network.n['Meth'])
+    addNTC = model.addVars(Pedges,vtype=GRB.CONTINUOUS,obj=cNTC) 
+    addMH = model.addVars(Hedges,vtype=GRB.CONTINUOUS,obj=cMH) 
+    
+    HtE = model.addVars(product(scenarios,time_steps,nodes),vtype=GRB.CONTINUOUS, obj=chte/d,lb=0) # expressed in kg      
+    EtH = model.addVars(product(scenarios,time_steps,nodes),vtype=GRB.CONTINUOUS, obj=ceth/d, lb=0) # expressed in MWh
+    H = model.addVars(product(scenarios,time_steps,nodes),vtype=GRB.CONTINUOUS,lb=0)
+    P_edge = model.addVars(product(scenarios,time_steps,Pedges),vtype=GRB.CONTINUOUS,lb=-GRB.INFINITY) #could make sense to sosbstitute Nodes with Network.nodes and so on Nedges with n.edgesP['start_node'],n.edgesP['end_node'] or similar
+    print(P_edge.keys())
+    #fai due grafi diversi
+    H_edge = model.addVars(product(scenarios,time_steps,Hedges),vtype=GRB.CONTINUOUS,lb=-GRB.INFINITY)
+
+    #todo: add starting capacity for generators (the same as for liners)
+    model.addConstrs( H[j,t,k] <= nh[k] for t in time_steps for j in scenarios for k in nodes) 
+    model.addConstrs( EtH[j,t,k] <= meth[k] for t in time_steps for j in scenarios for k in nodes)
+    model.addConstrs( HtE[j,t,k] <= mhte[k] for t in time_steps for j in scenarios for k in nodes)
+    model.addConstrs( P_edge[j,t,(n0,n1)] <= Network.edgesP['NTC'] + addNTC[(n0,n1)] for t in time_steps for j in scenarios for (n0,n1) in Pedges)
+    model.addConstrs( H_edge[j,t,(n0,n1)] <= Network.edgesH['MH'].iloc[k] + addMH[k] for t in time_steps for j in scenarios for (n0,n1) in Hedges)
+
+    outputs=[]
+    VARS=[]
+    #todo perchè lo rimuoviamo questo?
+    cons1=model.addConstrs(nh[n]>=0 for n in nodes )#for j in range(d) for i in time_steps)
+    #todo: sobstitute 30 with a parameter
+    cons2=model.addConstrs(  H[j,t,k] - H[j,t+1,n] + 30*Network.n['feth'].iloc[k]*EtH[j,t,k] - HtE[j,t,k] -  
+                           quicksum(H_edge[j,t,(n,m)] for m in nodes if (m,n) in Hedges) +
+                           quicksum(H_edge[j,t,(m,n)] for m in nodes if (n,m) in Hedges)
+                           ==0 for j in scenarios for t in time_steps[:-1] for n in nodes)
+    cons3=model.addConstrs(- H[j,0,n] + H[j,inst-1,n] + 30*Network.n['feth'].iloc[n]*EtH[j,inst-1,n] - HtE[j,inst-1,n] -
+                           quicksum(H_edge[j,inst-1,(n,m)] for m in nodes if (n,m) in Hedges) +
+                           quicksum(H_edge[j,inst-1,(m,n)] for m in nodes if (m,n) in Hedges)
+                           ==0 for j in scenarios for n in nodes)
+    print('OPT Model has been set up, this took ',np.round(time.time()-start_time,4),'s.')
+    
+    for group in range(rounds):
+        gr_start_time=time.time()
+
+    
+        ES = Network.genS_t.sel(scenario = slice(d*group, (d+1)*group))
+        EW = Network.genW_t.sel(scenario = slice(d*group, (d+1)*group))
+        EL = Network.loadE_t.sel(scenario = slice(d*group, (d+1)*group))
+        HL = Network.loadH_t.sel(scenario = slice(d*group, (d+1)*group))
+
+        model.remove(cons1)
+        for j in scenarios: 
+            for k in nodes:
+                for i in range(inst-1):
+                    cons2[j,i,k].rhs = HL[i,k,j] #time,node,scenario or if you prefer to not remember use isel     
+                cons3[j,k].rhs  = HL[inst-1,k,j]
+        
+        try:    
+            cons1=model.addConstrs(ns[n]*ES[t,n,j] + nw[n]*EW[t,n,j] + 0.033*Network.n['fhte'].iloc[n]*HtE[j,t,n] - EtH[j,t,n] -
+                                quicksum(P_edge[j,t,(n,m)] for m in nodes if (m,n) in Pedges) +
+                                quicksum(P_edge[j,i,(m,n)] for m in nodes if (n,m) in Pedges) 
+                                >= EL[i,k,j] for n in nodes for j in scenarios for t in time_steps)
+        except IndexError as e:
+            print(f"IndexError occurred at t={t}, n={n}, j={j}")
+            print(f"ES shape: {ES.shape}")
+            print(f"EW shape: {EW.shape}")
+            print(f"HtE shape: {HtE.shape}")
+            print(f"EtH shape: {EtH.shape}")
+            print(f"P_edge shape: {P_edge.shape}")
+            print(f"Network.n indices: {Network.n.index.to_list()}")
+            print(f"Network.edgesP start_node indices: {Network.edgesP['start_node'].index.to_list()}")
+            print(f"Network.edgesP end_node indices: {Network.edgesP['end_node'].index.to_list()}")
+            raise e  # Re-raise the exception after logging the details
+        
+        model.optimize()
+        if model.Status!=2:
+            print("Status = {}".format(model.Status))
+        else:
+            VARS=[np.ceil([ns[k].X for k in nodes]),np.ceil([nw[k].X for k in nodes]),np.ceil([nh[k].X for k in nodes]),np.ceil([mhte[k].X for k in nodes]),np.ceil([meth[k].X for k in nodes])]       
+            outputs=outputs + [VARS+[model.ObjVal]] 
+            print("Round {} of {} - opt time: {}s.".format(group+1,rounds, np.round(time.time()-gr_start_time,3)))
+            
+    return outputs#,HH,ETH,HTE
+
+
+# %% 
+results = OPT3(eu)
 # %%
